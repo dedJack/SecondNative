f386f7231ca930d29ede1948fbd351cfa9bfaccb
Aman Choudhary <142047102+dedJack@users.noreply.github.com>
2025-04-08 12:44:24 +0530

    Applied more animations property such as translate, scale.

--- src/Animations/FadeDemo.tsx
+++ src/Animations/FadeDemo.tsx
@@ -1,10 +1,21 @@
 import React, {useRef} from 'react';
-import {Animated, StyleSheet, Text, TouchableOpacity, View, ScrollView} from 'react-native';
+import {
+  Animated,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ScrollView,
+  Easing,
+} from 'react-native';
+import AnimationDemo from './AnimationDemo';
 
 const FadeDemo: React.FC = () => {
-
   //useRef is used to keep values and not change during re-renders.
   const fadeAnimation = useRef(new Animated.Value(0)).current;
+  const transAnimation = useRef(new Animated.Value(0)).current;
+  const scaleAnimationX = useRef(new Animated.Value(0)).current;
+  const scaleAnimation = useRef(new Animated.Value(0)).current;
 
   const handleFadeIn = () => {
     //Animated.timing() is use d for smooth tranistions in animation
@@ -21,16 +32,51 @@ const FadeDemo: React.FC = () => {
       useNativeDriver: true,
     }).start();
   };
+
+  const handleTranslate = () => {
+    Animated.timing(transAnimation, {
+      toValue: 100,
+      duration: 1000,
+      easing: Easing.bezier(0.42, 0, 0.58, 1),
+      useNativeDriver: true,
+    }).start();
+  };
+  const handleTranslate1 = () => {
+    Animated.timing(transAnimation, {
+      toValue: 0,
+      duration: 1000,
+      easing: Easing.bezier(0.42, 0, 0.58, 1),
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const handleScale = () => {
+    Animated.sequence([
+      Animated.timing(scaleAnimation, {
+        toValue: 1.5,
+        duration:1000,
+        useNativeDriver:true
+      }),
+      Animated.timing(scaleAnimation, {
+        toValue: .25,
+        duration:1000,
+        useNativeDriver:true
+      }),
+      Animated.timing(scaleAnimation, {
+        toValue: 1,
+        duration:1000,
+        useNativeDriver:true
+      }),
+    ]).start();
+  };
+
   return (
     <ScrollView contentContainerStyle={styles.container}>
+      {/* Fade Demo Animation */}
       <Text style={styles.header}>Fade Demo</Text>
-
       {/* It is used for animations */}
       <Animated.View
-        style={[
-          styles.box,
-          {opacity: fadeAnimation},
-        ]}></Animated.View>
+        style={[styles.box, {opacity: fadeAnimation}]}></Animated.View>
       <View style={styles.btnContainer}>
         <TouchableOpacity onPress={handleFadeIn} style={styles.btn}>
           <Text style={styles.btnText}>Fade INN</Text>
@@ -39,14 +85,55 @@ const FadeDemo: React.FC = () => {
           <Text style={styles.btnText}>Fade OUT</Text>
         </TouchableOpacity>
       </View>
+
+      {/* Translate Demo */}
+      <Text style={styles.header}>Translate Demo</Text>
+      <Animated.View
+        style={[
+          styles.box,
+          {
+            // Transform property accepts an array of transformation object
+            transform: [
+              {
+                translateX: transAnimation,
+              },
+            ],
+          },
+        ]}></Animated.View>
+      <View style={styles.btnContainer}>
+        <TouchableOpacity style={styles.btn} onPress={handleTranslate}>
+          <Text style={styles.btnText}>Translate</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn} onPress={handleTranslate1}>
+          <Text style={styles.btnText}>Translate reverse</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/*Scale demo*/}
+      <Text style={styles.header}>Scale Demo</Text>
+      <Animated.View
+        style={[
+          styles.box,
+          {
+            transform: [
+              {
+                scale: scaleAnimation,
+              },
+            ],
+          },
+        ]}></Animated.View>
+      <View style={styles.btnContainer}>
+        <TouchableOpacity style={styles.btn} onPress={handleScale}>
+          <Text style={styles.btnText}>Scale</Text>
+        </TouchableOpacity>
+      </View>
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    marginBottom: 20,
+    paddingBottom: 100,
     alignItems: 'center',
   },
   header: {
@@ -54,10 +141,11 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   box: {
+    marginLeft: 10,
     borderRadius: 8,
     borderWidth: 1,
-    height: 300,
-    width: 300,
+    height: 200,
+    width: 200,
     marginVertical: 20,
     shadowColor: 'black',
     shadowOffset: {
